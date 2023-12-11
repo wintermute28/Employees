54f7b3baba85cff7c41546536f278a7b007a1a2c
wintermute28 <a28c@yandex.ru>
2023-12-11 20:03:59 +0300

    Add delete & add employees function

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -17,27 +17,39 @@ class EmployeesAddForm extends Component{
         })
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
   
     
     render() {
         const {name, salary} = this.state;
+
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit = {this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
                         name="name"
-                        value={name}
-                        onChange={this.onValueChange} />
+                        value={name} 
+                        onChange={this.onValueChange}/>
                     <input type="number"
                         className="form-control new-post-label"
                         placeholder="З/П в $?"
                         name="salary"
-                        value={salary}
-                        onChange={this.onValueChange} />
+                        value={salary} 
+                        onChange={this.onValueChange}/>
+    
                     <button type="submit"
                             className="btn btn-outline-light">Добавить</button>
                 </form>
