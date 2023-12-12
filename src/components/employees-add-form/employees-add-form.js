665d4727cf62b80ff3f87ab7a511e706b83fe6d7
wintermute28 <a28c@yandex.ru>
2023-12-12 12:11:03 +0300

    Add form validation

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -19,6 +19,7 @@ class EmployeesAddForm extends Component{
 
     onSubmit = (e) => {
         e.preventDefault();
+        if (this.state.name.length < 3 || !this.state.salary) return;
         this.props.onAdd(this.state.name, this.state.salary);
         this.setState({
             name: '',
@@ -42,11 +43,13 @@ class EmployeesAddForm extends Component{
                         placeholder="Как его зовут?"
                         name="name"
                         value={name} 
+                        required
                         onChange={this.onValueChange}/>
                     <input type="number"
                         className="form-control new-post-label"
                         placeholder="З/П в $?"
                         name="salary"
+                        required
                         value={salary} 
                         onChange={this.onValueChange}/>
     
